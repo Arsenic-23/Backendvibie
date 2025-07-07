93811725e8e325b93c65025524b6079107a1d230
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-07 18:47:06 +0530

    Create suggest.py

---
+++ routers/suggest.py
@@ -0,0 +1,14 @@
+rom fastapi import APIRouter, Query
+from typing import List
+from youtubesearchpython import Suggestions
+
+router = APIRouter(prefix="/suggest", tags=["Suggestions"])
+
+@router.get("/")
+def suggest(q: str = Query(...)) -> dict:
+    try:
+        suggestion_result = Suggestions(q).get()
+        suggestions = suggestion_result.get("suggestions", [])
+        return {"results": suggestions[:10]}
+    except Exception as e:
+        return {"results": [], "error": str(e)}
